c9152711f59c54dbb90f7ddc86fbd3fcd80a57ed
Debojit <debojitchoudhury117@gmail.com>
2025-02-20 07:45:30 +0000

    changed debug and host

--- clinic360/settings.py
+++ clinic360/settings.py
@@ -26,9 +26,9 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
